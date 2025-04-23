723a941a180470f9d1facfb4b736bc0c5ff7a6e4
TheKneecapKnabber <158204152+TheKneecapKnabber@users.noreply.github.com>
2025-04-23 13:50:58 -0500

    made the static disc object

--- Assets/Scripts/Weapons/ThrowableProjectile.cs
+++ Assets/Scripts/Weapons/ThrowableProjectile.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class ThrowableProjectile : projectileBase
 {
+    [SerializeField] private GameObject staticObject;//object that gets spawned to stick to the collided object
     protected override void OnCollisionEnter(Collision other)
     {
         if (other.collider != null && other.collider.gameObject.GetComponent<IDamageable>() != null)
         {
             DealDamage(other);
         }
+
         /*
         if (other.gameObject.GetComponent<Rigidbody>() != null)
         {
@@ -22,7 +24,7 @@ public class ThrowableProjectile : projectileBase
             
         }
         */
-        transform.SetParent(other.transform,  true);
+        transform.SetParent(other.transform.root,  true);
         //rb.useGravity = false;
     }
     
